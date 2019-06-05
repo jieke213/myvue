0d587f59ce4c8296068faed116d352d9355a4add
WangJingjie <wangjjj@sunits.com>
2019-06-06 00:04:16 +0800

    v-if v-else v-show

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,23 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    {{msg}}
+    <br>
+    {{1>2 ? "是":"不是"}}
+
+    <p class="ha">哈哈哈</p>
+    <p v-bind:class="ha">呵呵呵</p>
+
+    {{ ok ? "正确":"错误" }}<br>
+
+    <template v-if="ok">
+      <span>我是ok为true时显示的</span>
+    </template>
+    <template v-else="">
+      <span>我是ok为false时显示的</span>
+    </template>
+    <br>
+
+    <p v-show="flag">flag为true时显示</p>
   </div>
 </template>
 
@@ -88,7 +26,10 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '王景杰',
+      ha: "哈",
+      ok: false,
+      flag: true
     }
   }
 }
